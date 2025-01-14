0f46118b7b176d8997c0330be31e8742d6f6f016
Hasnaoui-Youssef <hasneouiyoussef@gmail.com>
2025-01-15 00:01:03 +0100

    badalna

--- src/monthly/monthly.controller.ts
+++ src/monthly/monthly.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Inject } from '@nestjs/common';
+import { Body, Controller, Inject, Post } from '@nestjs/common';
 import { DateDTO } from 'src/common/dto/date-query.dto';
 import { MonthlyService } from './monthly.service';
 
@@ -6,7 +6,7 @@ import { MonthlyService } from './monthly.service';
 export class MonthlyController {
     constructor(@Inject() private readonly monthlyService : MonthlyService){}
 
-    @Get()
+    @Post()
     getMonthlyReport(@Body() dateDTO : DateDTO){
         return this.monthlyService.getMonthlyReport(dateDTO.month, dateDTO.year);
     }
